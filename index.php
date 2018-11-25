d3c6668f4cb5dca205da7108604033ee0328af01
Michael Hillcox <hello.michaelhillcox@gmail.com>
2018-11-25 14:04:57 +0000

    Added the new dom structure

--- index.php
+++ index.php
@@ -4,50 +4,40 @@
 		<meta charset="utf-8">
 		<title>MiKeY</title>
 
-		<link href="https://fonts.googleapis.com/css?family=Lato:300,100" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Lato:300" rel="stylesheet">
 		<link rel="stylesheet" type="text/css" href="app/core.min.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+
 		<meta name="description" content="The nexus of MiKeY ( Also know as AoKMiKeY ). All of my contacts and work in one place">
-		<meta name="keywords" content="aokmikey,AoKMiKeY,aokmikeyv2,mikey,se7ensins aokmikey,cod5,cod4,cod 5 modding,cod 4 modding,aokmikey youtube,aokmikey se7ensins,agreedbog,perfectionist mod menu,design,development,raid-gaming,raid gaming,raid-server,raid servers">
+		<meta name="keywords" content="aokmikey,AoKMiKeY,aokmikeyv2,mikey,se7ensins aokmikey,cod5,cod4,cod 5 modding,cod 4 modding,aokmikey youtube,aokmikey se7ensins,agreedbog,perfectionist mod menu,design,development,zombiemodding,zm,zombiemodding.com">
 	</head>
-	<body <?= isset( $_GET['p'] ) ? 'class="discord"' : '' ?> >
-		<?php if( !isset( $_GET['p'] ) ): ?>
-		<div id="content">
-			<header>
-				<h1>MiKeY</h1>
-				<h2>Designer, Developer, Modder and Creator.</h2>
-			</header>
-			<footer>
-				<?php
-					$jsonFile = file_get_contents("app/social.json");
-					$socialLinks = json_decode($jsonFile);
+	<body>
+		<div class="wrapper">
+			<main class="main">
+				<div class="logo"></div>
+				<section>
+					<header>
+						<h1><small>Hey, I'm</small> MiKeY</h1>
+					</header>
+					<div class="body">
+						<p>Modder of <a href="#">CoD5</a>, Maintainer of <a href="">ZombieModding</a>,</p>
+						<p>Developer, Designer, Creator.</p>
 
-					foreach ($socialLinks as $link):
-						foreach ($link as $data => $moreData ):
-
-							echo "<a href='{$moreData->link}' title='".htmlentities($moreData->title)."'>";
-							if( isset($moreData->icon) )
-								echo "<i class='icon {$moreData->icon}'></i>";
-							else
-								echo "<div class='{$data} socialItem'></div>";
-							echo "</a>";
-
-						endforeach;
-					endforeach;
-
-				?>
-			</footer>
+						<small class="subtile">Socials</small>
+						<div class="links">
+							<a data-email>Email</a>
+							<a data-discord>Discord</a>
+							<a href="#">Github</a>
+							<a href="#">Twitter</a>
+							<a href="#">Youtube</a>
+							<a href="#">Steam</a>
+						</div>
+					</div>
+				</section>
+			</main>
 		</div>
-
-		<?php else: ?>
-			
-			<div class="d-container">
-				<div class="d-box">
-					<h2>Add me on Discord</h2>
-					<p>For some reason Discord doesn't support direct linking to my profile so you'll have to do it manually. Sorry... I guess?</p>
-					<div class="d-input">MiKeY#3704<a class="d-btn" href="/">Back</a></div>
-				</div>
-			</div>
-		<?php endif; ?>
+		<footer>
+			<p>&copy; <?= date("Y") ?> MiKeY</p>
+		</footer>
 	</body>
 </html>
